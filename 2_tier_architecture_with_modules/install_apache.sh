cb7a5432cda58a983121453a2f7be7fca6a98d32
rev <rev@example.com>
2022-08-23 02:38:13 +0000

    adding files

---
+++ 2_tier_architecture_with_modules/install_apache.sh
@@ -0,0 +1,10 @@
+# --- Terraform_projects/2_tier_architecture_with_modules/install_apache.sh ---
+
+
+#!/bin/bash
+
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
---
+++ 2_tier_architecture_with_modules/outputs.tf
@@ -0,0 +1,6 @@
+# --- Terraform_projects/2_tier_architecture_with_modules/outputs.tf ---
+
+output "alb_dns" {
+  value = module.loadbalancing.alb_dns
+}
+
