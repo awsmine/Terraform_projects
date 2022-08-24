f20176e51315862d2df30e53421153a383dcb2ef
rev <rev.example.com>
2022-08-24 21:52:52 +0000

    r

--- basic_ec2_module_1/outputs.tf
+++
@@ -1,9 +0,0 @@
-# --- Terraform_projects/basic_ec2_module_1/outputs.tf ---
-
-output "ec2_public_ip" {
-  value = module.ec2_instance.ec2_public_ip
-}
-
-output "my_ec2_name" {
-  value = module.ec2_instance.my_ec2_name
-}
