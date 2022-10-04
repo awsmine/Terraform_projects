4b2ca9faf04e02847d9cfed57d6c72975a02c4a7
rev <rev@example.com>
2022-10-04 14:21:38 +0000

    removed

--- basic_ec2_module_1/ec2_instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-# --- Terraform_projects/basic_ec2_module_1/ec2_instance/outputs.tf ---
-
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
-}
-
-output "my_ec2_name" {
-  value = aws_instance.my_ec2.tags_all.Name
-}
